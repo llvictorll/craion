a3083767392bcaf9b84e918bc9fa6d0c61e043e1
llvictorll <victor@PRA1-L13006.vnet.valeo.com>
2024-02-07 17:55:18 +0100

    add front fliiiip

--- scripts.js
+++ scripts.js
@@ -1,13 +1,26 @@
-function rotateImage() {
+function rotateImage1() {
     const generatedImage = document.getElementById('TheFrog');
 
     // Apply the 'rotate' class to trigger the CSS animation
-    generatedImage.classList.add('rotate');
+    generatedImage.classList.add('rotate1');
 
     // Remove the 'rotate' class after the animation duration to reset for the next click
     setTimeout(() => {
-        generatedImage.classList.remove('rotate');
+        generatedImage.classList.remove('rotate1');
     }, 1000); // Adjust the duration (in milliseconds) to match the CSS animation duration
 }
 
-document.getElementById('rotateButton').addEventListener('click', rotateImage);
+function rotateImage2() {
+    const generatedImage = document.getElementById('TheFrog');
+
+    // Apply the 'rotate' class to trigger the CSS animation
+    generatedImage.classList.add('rotate2');
+
+    // Remove the 'rotate' class after the animation duration to reset for the next click
+    setTimeout(() => {
+        generatedImage.classList.remove('rotate2');
+    }, 1000); // Adjust the duration (in milliseconds) to match the CSS animation duration
+}
+
+document.getElementById('rotateButton1').addEventListener('click', rotateImage1);
+document.getElementById('rotateButton2').addEventListener('click', rotateImage2);
